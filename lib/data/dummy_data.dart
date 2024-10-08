58cb56af615126abbea0a946bf686e6f983e25b3
stenOskar <stenhalse@gmail.com>
2024-10-08 21:16:43 +0200

    Implemented skeleton of meals app

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,35 @@
+import 'package:meals_app/models/category.dart';
+import 'package:flutter/material.dart';
+
+const availableCategories = [
+  Category(
+      id: "c1",
+      title: "Italian",
+      color: Colors.purple,
+  ),
+  Category(
+    id: "c2",
+    title: "Quick and easy",
+    color: Colors.red,
+  ),
+  Category(
+    id: "c3",
+    title: "Hamburgers",
+    color: Colors.orange,
+  ),
+  Category(
+    id: "c4",
+    title: "Swedish",
+    color: Colors.yellow,
+  ),
+  Category(
+    id: "c5",
+    title: "Norwegian",
+    color: Colors.blue,
+  ),
+  Category(
+    id: "c6",
+    title: "Japanese",
+    color: Colors.red,
+  ),
+];
