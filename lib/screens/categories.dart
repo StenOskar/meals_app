94dd0265d1e6d057b9ae3d445b2e6f22e8bc9316
stenOskar <stenhalse@gmail.com>
2024-10-10 18:06:27 +0200

    Implemented new variables for filtering and navigation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,28 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
-import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/models/category.dart';
+import '../models/meal.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({
+    super.key,
+    required this.onToggleFavorite,
+    required this.availableMeals,
+  });
+
+  final void Function(Meal meal) onToggleFavorite;
+  final List<Meal> availableMeals;
 
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = dummyMeals
+    final filteredMeals = availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
-    Navigator.push(
-      context,
+    Navigator.of(context).push(
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          meals: filteredMeals,
           title: category.title,
+          meals: filteredMeals,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
-    );
+    ); // Navigator.push(context, route)
   }
 
   @override
@@ -36,12 +42,14 @@ class CategoriesScreen extends StatelessWidget {
         mainAxisSpacing: 20,
       ),
       children: [
-        ...availableCategories.map((category) => CategoryGridItem(
-              category: category,
-              onSelectCategory: () {
-                _selectCategory(context, category);
-              },
-            ))
+        // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+        for (final category in availableCategories)
+          CategoryGridItem(
+            category: category,
+            onSelectCategory: () {
+              _selectCategory(context, category);
+            },
+          )
       ],
     );
   }
